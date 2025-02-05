2b56dbe60451f640b8faf7acb018fecf7b994368
Your Name <you@example.com>
2025-02-05 22:23:58 +0100

    Commencement de la page d'acceuil header et commencement du  footer

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,24 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';  // Importation de `ref` pour la réactivité
 
-// Déclaration d'une variable réactive
-const message = ref('Bienvenue sur la page d\'accueil');
 </script>
 
 <template>
-  <div>
-    <!-- Affichage du message -->
-    <h1>{{ message }}</h1>
-    <p>Voici le contenu de la page d'accueil sans image.</p>
-  </div>
+  <main>
+
+  </main>
 </template>
-
-<style scoped>
-h1 {
-  color: #42b983;
-}
-
-p {
-  font-size: 18px;
-}
-</style>
